eda05b95faed45761fc250724e7dc9573d23664b
ThangNguyen <thangnv1029@gmail.com>
2025-07-15 23:39:07 +0700

    Update eslint to clean code for project

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -1,23 +1,57 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
-
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+import reactPlugin from 'eslint-plugin-react'
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ...reactPlugin.configs.flat.recommended,
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      reactPlugin.configs.flat.recommended, // Bật rule React phổ biến (bao gồm 'display-name', 'jsx-key', ...)
+      reactPlugin.configs.flat['jsx-runtime'] // Dành cho React 17+ (React 18), vô hiệu hoá rule đã lỗi thời (ví dụ import React)
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
-  },
-]);
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 1,
+      'react/display-name': 0,
+
+      'no-console': 1,
+      'no-lonely-if': 1,
+      'no-unused-vars': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      indent: ['warn', 2],
+      semi: [1, 'never'],
+      quotes: ['error', 'single'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-dangle': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1
+    },
+    settings: {
+      'react': {
+        'version': 'detect'
+      }
+    }
+  }
+])
