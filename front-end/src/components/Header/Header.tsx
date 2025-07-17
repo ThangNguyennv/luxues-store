b26fb4d7f1d3d02e16616b6c50548876641ab849
ThangNguyen <thangnv1029@gmail.com>
2025-07-17 16:44:04 +0700

    Update header, section of home in front-end

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,7 +1,64 @@
-const header123 = () => {
+import { IoMdClose } from 'react-icons/io'
+import { IoSearch } from 'react-icons/io5'
+import { IoMdCart } from 'react-icons/io'
+import { FaRegUserCircle } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
+
+const Header = () => {
   return (
-    <div><h1>Header123</h1></div>
+    <>
+      {/* Top header */}
+      <div className="bg-primary sm:py-[10px] py-[9px]">
+        <div className="container mx-auto px-[16px]">
+          <div className="flex">
+            <div className="text-white flex-1 text-center sm:text-[14px] text-[12px]">
+              <span className="font-[400]">Đăng ký để được giảm giá 20%.</span>
+              <a className="font-[500] underline ml-[5px]" href="#">Đăng Ký Ngay</a>
+            </div>
+            <button className="text-white text-[14px] sm:inline-block hidden"><IoMdClose /></button>
+          </div>
+        </div>
+      </div>
+      {/* End Top header */}
+      {/* Header */}
+      <header className="sm:py-[24px] py-[20px]">
+        <div className="container mx-auto px-[16px]">
+          <div className="flex items-center md:gap-x-[40px] gap-x-[16px]">
+            <button className=" text-[20px] md:hidden inline">
+              <FaBars />
+            </button>
+            <a className="font-[700] sm:text-[32px] text-[25px] text-primary lg:flex-none flex-1" href="">CODE.SHOP</a>
+            <nav className="md:block hidden">
+              <ul className="flex gap-x-[24px] font-[400] text-[16px] text-black">
+                <li><a href="">Giới Thiệu</a></li>
+                <li><a href="">Sản Phẩm</a></li>
+                <li><a href="">Bài Viết</a></li>
+                <li><a href="">Liên Hệ</a></li>
+              </ul>
+            </nav>
+            <form className="flex-1 lg:flex hidden items-center gap-x-[12px] px-[16px] py-[12px] bg-[#F0F0F0] rounded-[62px] text-[16px]" action="#">
+              <button className="text-[#00000066]">
+                <IoSearch />
+              </button>
+              <input className="bg-transparent flex-1" type="" placeholder="Tìm kiếm sản phẩm..."/>
+            </form>
+            <div className="flex items-center gap-x-[14px] text-[21px]">
+              <a className="lg:hidden inline" href="#">
+                <IoSearch />
+              </a>
+              <a href="#">
+                <IoMdCart />
+              </a>
+              <a href="#">
+                <FaRegUserCircle />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* End Header */}
+    </>
   )
 }
 
-export default header123
+export default Header
--- front-end/src/pages/Home/Home.tsx
+++ front-end/src/pages/Home/Home.tsx
@@ -1,6 +1,48 @@
+import backgroundHome from '~/assets/images/Home/background-home.png'
+import star from '~/assets/images/Home/star.svg'
+
 const Home = () => {
   return (
-    <div><h1>Home</h1></div>
+    <>
+      {/* Section 1 */}
+      <div className="bg-[#F2F0F1] truncate">
+        <div className="container mx-auto px-[16px] whitespace-normal">
+          <div className="flex flex-wrap items-center" data-aos="fade-up" data-aos-duration="2000">
+            <div className="lg:w-[48.6%] w-[100%] lg:mt-[0] mt-[40px]">
+              <h1 className="font-[900] xl:text-[48px] sm:text-[32px] text-[28px] text-primary mb-[10px]" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="300">
+                CHUYÊN THỜI TRANG PHONG CÁCH, HIỆN ĐẠI
+              </h1>
+              <p className="font-[400] xl:text-[16px] text-[14px] text-[#00000099] xl:mb-[50px] mb-[30px]" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="200">
+                Chúng tôi chuyên cung cấp nhiều loại quần áo được chế tác tỉ mỉ, được thiết kế để làm nổi bật cá tính của bạn và đáp ứng phong cách của bạn.
+              </p>
+              <a className="sm:inline-block block text-center bg-primary rounded-[62px] py-[16px] px-[64px] font-[500] text-[16px] text-white" href="#" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="150">
+                Xem Ngay
+              </a>
+              <div className="flex flex-wrap xl:mt-[50px] mt-[30px] sm:justify-start justify-center sm:text-left text-center sm:gap-[0] gap-x-[75px] gap-y-[10px]">
+                <div className="sm:pr-[32px] sm:border-r border-[#0000001A] sm:mr-[32px]">
+                  <div className="text-primary xl:text-[40px] text-[24px] font-[700]">200+</div>
+                  <div className="font-[400] xl:text-[16px] text-[12px] text-[#00000099]">Thương Hiệu</div>
+                </div>
+                <div className="sm:pr-[32px] sm:border-r border-[#0000001A] sm:mr-[32px]">
+                  <div className="text-primary xl:text-[40px] text-[24px] font-[700]">2,000+</div>
+                  <div className="font-[400] xl:text-[16px] text-[12px] text-[#00000099]">Sản Phẩm Chất Lượng</div>
+                </div>
+                <div className="sm:pr-[32px] sm:border-r border-[#0000001A] sm:mr-[32px]">
+                  <div className="text-primary xl:text-[40px] text-[24px] font-[700]">30,000+</div>
+                  <div className="font-[400] xl:text-[16px] text-[12px] text-[#00000099]">Khách Hàng</div>
+                </div>
+              </div>
+            </div>
+            <div className="relative xl:ml-[60px] lg:flex-1 flex-none w-[100%]" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="150">
+              <img className="w-[100%]" src={ backgroundHome } alt="bg-section1"/>
+              <img className="w-[56px] absolute left-[0] top-[48%] sm:block hidden" src={ star }/>
+              <img className="w-[104px] absolute right-[0] top-[14%] sm:block hidden" src={ star }/>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Section 1 */}
+    </>
   )
 }
 
