ce2cf73b5ea7e8c1f75ba7015e2d12d1871da62b
ThangNguyen <thangnv1029@gmail.com>
2025-09-12 20:50:49 +0700

    Update pagination in back-end

--- back-end/src/controllers/admin/order.controller.ts
+++ back-end/src/controllers/admin/order.controller.ts
@@ -39,7 +39,7 @@ export const index = async (req: Request, res: Response) => {
     const objectPagination = paginationHelpers(
       {
         currentPage: 1,
-        limitItems: 10
+        limitItems: 3
       },
       req.query,
       countOrders
