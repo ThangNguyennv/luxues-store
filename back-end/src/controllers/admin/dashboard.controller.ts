527abae16aabf9f38d974aa055455789784cb4e4
ThangNguyen <thangnv1029@gmail.com>
2025-09-18 17:40:03 +0700

    Update controller dashboard

--- back-end/src/controllers/admin/dashboard.controller.ts
+++ back-end/src/controllers/admin/dashboard.controller.ts
@@ -1,82 +1,46 @@
 import { Request, Response } from 'express'
-import CategoryProduct from '~/models/product-category.model'
 import Product from '~/models/product.model'
-import Account from '~/models/account.model'
 import User from '~/models/user.model'
+import Order from '~/models/order.model'
 
 // [GET] /admin/dashboard
 export const dashboard = async (req: Request, res: Response) => {
   try {
     const statistic = {
-      categoryProduct: {
+      user: {
         total: 0,
-        active: 0,
-        inactive: 0
       },
       product: {
         total: 0,
-        active: 0,
-        inactive: 0
       },
-      account: {
+      order: {
         total: 0,
-        active: 0,
-        inactive: 0
       },
-      user: {
+      revenue: {
         total: 0,
-        active: 0,
-        inactive: 0
       }
     }
+    
+    const orders = await Order.find({
+      'paymentInfo.status': 'PAID'
+    })
 
-    statistic.categoryProduct.total = await CategoryProduct.countDocuments({
+    const totalRevenue = orders.reduce((sum, order) => sum + order.amount, 0)
+
+    statistic.user.total = await User.countDocuments({
       deleted: false
-    })
-    statistic.categoryProduct.active = await CategoryProduct.countDocuments({
-      deleted: false,
-      status: 'active'
-    })
-    statistic.categoryProduct.inactive = await CategoryProduct.countDocuments({
-      deleted: false,
-      status: 'inactive'
     })
 
     statistic.product.total = await Product.countDocuments({
       deleted: false
     })
-    statistic.product.active = await Product.countDocuments({
-      deleted: false,
-      status: 'active'
-    })
-    statistic.product.inactive = await Product.countDocuments({
-      deleted: false,
-      status: 'inactive'
-    })
 
-    statistic.account.total = await Account.countDocuments({
+    statistic.order.total = await Order.countDocuments({
       deleted: false
     })
-    statistic.account.active = await Account.countDocuments({
-      deleted: false,
-      status: 'active'
-    })
-    statistic.account.inactive = await Account.countDocuments({
-      deleted: false,
-      status: 'inactive'
-    })
 
-    statistic.user.total = await User.countDocuments({
-      deleted: false
-    })
-    statistic.user.active = await User.countDocuments({
-      deleted: false,
-      status: 'active'
-    })
-    statistic.user.inactive = await User.countDocuments({
-      deleted: false,
-      status: 'inactive'
-    })
+    statistic.revenue.total = totalRevenue
+
     res.json({
       code: 200,
       message: 'Thành công!',
