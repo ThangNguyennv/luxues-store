78644a998a81772937b06bcc4b1448e7b0cbd8bd
ThangNguyen <thangnv1029@gmail.com>
2025-09-21 23:51:11 +0700

    Update dashboard

--- front-end/src/pages/Admin/Dashboard/Dashboard.tsx
+++ front-end/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -51,18 +51,11 @@ const Dashboard = () => {
         <h1 className="text-[30px] font-[700] text-[#BC3433]">Thống kê</h1>
 
         <div className='grid grid-cols-4 gap-[10px] p-[5px] text-amber-50'>
-          <div className='border rounded-[5px] p-[15px] gap-[25px] flex items-center justify-center bg-[#2F57EF]'>
-            <PiUserListBold className='text-[30px]'/>
+          <div className='border rounded-[5px] p-[15px] gap-[25px] flex items-center justify-center bg-[#FFAB19]'>
+            <MdCheckCircleOutline className='text-[30px]'/>
             <div className='flex flex-col gap-[5px]'>
-              <b>Lượng truy cập</b>
-              <b>{statistic.user.total}</b>
-            </div>
-          </div>
-          <div className='border rounded-[5px] p-[15px] gap-[25px] flex items-center justify-center bg-[#525FE1]'>
-            <FaProductHunt className='text-[30px]'/>
-            <div className='flex flex-col gap-[5px]'>
-              <b>Sản phẩm</b>
-              <b>{statistic.product.total}</b>
+              <b>Doanh thu tháng {currentMonth}</b>
+              <b>{statistic.revenue.total.toLocaleString()}đ</b>
             </div>
           </div>
           <div className='border rounded-[5px] p-[15px] gap-[25px] flex items-center justify-center bg-[#18BA2A]'>
@@ -72,11 +65,18 @@ const Dashboard = () => {
               <b>{statistic.order.total}</b>
             </div>
           </div>
-          <div className='border rounded-[5px] p-[15px] gap-[25px] flex items-center justify-center bg-[#FFAB19]'>
-            <MdCheckCircleOutline className='text-[30px]'/>
+          <div className='border rounded-[5px] p-[15px] gap-[25px] flex items-center justify-center bg-[#525FE1]'>
+            <FaProductHunt className='text-[30px]'/>
             <div className='flex flex-col gap-[5px]'>
-              <b>Doanh thu tháng {currentMonth}</b>
-              <b>{statistic.revenue.total.toLocaleString()}đ</b>
+              <b>Sản phẩm</b>
+              <b>{statistic.product.total}</b>
+            </div>
+          </div>
+          <div className='border rounded-[5px] p-[15px] gap-[25px] flex items-center justify-center bg-[#2F57EF]'>
+            <PiUserListBold className='text-[30px]'/>
+            <div className='flex flex-col gap-[5px]'>
+              <b>Lượng truy cập</b>
+              <b>{statistic.user.total}</b>
             </div>
           </div>
         </div>
