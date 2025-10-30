7709fdffa512dacf8dc84c89f6e737d32d9c9af6
ThangNguyen <thangnv1029@gmail.com>
2025-10-30 12:43:02 +0700

    Update alert

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import '~/index.css'
 import App from '~/App'
 import CssBaseline from '@mui/material/CssBaseline'
-import { AlertToast } from '~/components/alert/Alert'
+import { AlertToast } from '~/components/Alert/Alert'
 import { AppProviders } from '~/AppProviders'
 
 createRoot(document.getElementById('root')!).render(
