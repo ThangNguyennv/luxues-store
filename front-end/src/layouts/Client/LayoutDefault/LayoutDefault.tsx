2e3f83d633ad917b51f3c3c4936e856434c9e333
ThangNguyen <thangnv1029@gmail.com>
2025-11-29 20:57:55 +0700

    No update

--- front-end/src/layouts/Client/LayoutDefault/LayoutDefault.tsx
+++ front-end/src/layouts/Client/LayoutDefault/LayoutDefault.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Header from '~/components/Client/Header/Header'
 import Footer from '~/components/Client/Footer/Footer'
-import ChatPage from '~/pages/Client/ChatPage/ChatPage'
+import ChatPage from '~/pages/Client/Chat/ChatPage'
 
 const LayoutDefault = () => {
   return (
